65ad73b3de5e7ae6665dc73ee29e379d4ac057b9
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-28 17:04:35 +0600

    Adding all files

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-fuchsia-800">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
