2d20d53b2e814db59500aab7796602c8fb88ee02
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-28 17:35:45 +0600

    Clerk authentication basic setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,20 @@
-import { Slot } from "expo-router";
-
-// Import your global CSS file
+import { tokenCache } from '@/cache'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
 import "../global.css";
 
-export default Slot;
+export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error('Add EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env')
+  }
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-fuchsia-800">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-fuchsia-800 text-3xl">Hi...</Text>
     </View>
   );
 }
